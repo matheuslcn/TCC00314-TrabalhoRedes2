e3c60ee57b3679e54f3bd4cd6807242bba2ef80c
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-12-07 16:29:08 -0300

    my mingraines are killing me, but I managed to start some procedural functions

--- CONTROL-SERV/utils.py
+++ CONTROL-SERV/utils.py
@@ -1,3 +1,113 @@
 from socket import *
+from collections import namedtuple
+
 import pandas
-import sqlalchemy
+import sqlalchemy as sql
+
+#---------------------------------------------------------------------
+# O objetivo das funçoes abaixo é de gerir a interação do servidor de
+# controle com a tabela de usuarios no servidor. Cada entrada dessa tabela
+# é indexada pelo seu hash, mas cada usuario prefere se identificar pelo
+# seu nome. Cada uma dessas funções també tem o argumento conn, que é um
+# sqlalchemy.Connection para o banco de dados do servidor.
+
+usr_tuple = namedtuple( 'usr_tuple' , [ 'nome' , 'hash' , 'premium' ] )
+
+def db_fetch_user( user_name , conn ):
+    
+    # o hash do nome é a chave primaria
+    h = hash( user_name )
+
+    pass
+
+def db_add_user( new_user , conn ):
+    pass
+
+def db_rmv_user( user_name , conn ):
+    pass
+
+def db_upgrade_user( user_name , conn ):
+    pass
+
+#---------------------------------------------------------------------
+# É interessante ter um cache de usuários na memória do servidor para
+# otimizar a performance das operações ao reduzir o numero de acessos
+# ao banco de dados.
+
+cache_usr_tuple = namedtuple( 'cache_usr_tuple' , [ 'usr_tuple' , 'altered' ] )
+
+def init_user_cache( num ):
+    
+    global user_cache
+    user_cache = dict()
+
+    global user_cache_limit
+    user_cache_limit = num
+
+def user_cache_full():
+    return len( user_cache ) >= user_cache_limit
+
+def cache_fetch_user( user_name ):
+    
+    # o hash do nome é a chave primaria
+    h = hash( user_name )
+    usr = user_cache.get( h , None )
+
+    if usr is None:
+        return None
+    
+    tup = usr.usr_tuple
+    if tup.nome != user_name:
+        return None
+    
+    return tup
+
+def cache_add_user( new_user , fetched = False ):
+    pass
+
+def cache_rmv_user( user_name ):
+    pass
+
+def cache_upgrade_user( user_name ):
+
+    pass
+
+#--------------------------------------------------------------------
+# Cada uma das demais tabelas tem suas funções equivalentes as da tabela
+# de usuarios. Aqui para tabela de grupos.
+
+group_tuple = namedtuple( 'group_tuple' , [ 'nome' , 'id' , 'dono' ] )
+
+def db_fetch_group( group_name, conn ):
+    
+    # o hash do nome é a chave primaria
+    h = hash( group_name )
+
+    pass
+
+def db_add_group( group_name, group_owner , conn ):
+    pass
+
+def db_rmv_group( group_name , conn ):
+    pass
+
+cache_group_tuple = namedtuple( 'cache_group_tuple' , [ 'group_tuple' , 'altered' ] )
+
+def init_group_cache( num ):
+    pass
+
+def group_cache_full():
+    pass
+
+def cache_fetch_group( group_name ):
+    
+    # o hash do nome é a chave primaria
+    h = hash( group_name )
+
+    pass
+
+def cache_add_group( group_name , owner ):
+    pass
+
+def cache_rmv_group( group_name ):
+    pass
