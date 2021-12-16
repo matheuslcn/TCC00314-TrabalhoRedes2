31bfe62d4a2f02f47562d53f60fd219e9a17f69a
matheuslcn <71265929+matheuslcn@users.noreply.github.com>
2021-12-16 14:15:46 -0300

    Comeco da comunicacao entre servidores e cliente

--- CONTROL-SERV/main.py
+++ CONTROL-SERV/main.py
@@ -1,9 +1,58 @@
 import socket
 
-HOST = '127.0.0.1'          # IP do servidor
-SERVER_PORT = 5000          # Porta onde o servidor está escutando
-STREAM_HOST = '127.0.0.1'   # IP do servidor de streaming
-STREAM_PORT = 5555          # Porta do servidor com o streaming
+HOST = '127.0.0.1'  # IP do servidor
+SERVER_PORT = 5000  # Porta onde o servidor está escutando
+STREAM_HOST = '127.0.0.1'  # IP do servidor de streaming
+STREAM_PORT = 5555  # Porta do servidor com o streaming
+
+
+def logout(user):
+    """
+    desloga o usuario
+    :param user:
+    :return 'SAIR_DA_APP_ACK':
+    """
+    print(user)
+    return 'SAIR_DA_APP_ACK'
+
+
+def get_user_information(user):
+    """
+    Pega todas as informacoes do usuario e envia para o servidor de streaming
+    :param user:
+    :return 'USER_INFORMATION' + ' ' + user_id:
+    """
+    print(user)
+    return 'USER_INFORMATION' + ' ' + 'user_id'
+
+
+def login(user):
+    """
+    Se achar o usuario no bd, manda a mensagem 'STATUS_DO_USUARIO',
+        informando ID, tipo de servico e membros do grupo.
+    Caso contrario, cria um no bd e envia 'ENTRAR_NA_APP_ACK' com uma mensagem de confirmacao da criacao.
+    :param user:
+    :return 'STATUS_DO_USUARIO' + ' ' + user_id || 'ENTRAR_NA_APP_ACK'  :
+    """
+    print(user)
+    return 'STATUS_DO_USUARIO' + ' ' + 'user_id'
+
+
+def connection_manager(conn):
+    with conn:
+        data_byte = conn.recv(1024)
+        data_string = data_byte.decode()
+        data = data_string.split(" ")
+
+        if data[0] == 'GET_USER_INFORMATION':
+            message = get_user_information(data[1])
+        elif data[0] == 'ENTRAR_NA_APP':
+            message = login(data[1])
+        elif data[0] == 'SAIT_DA_APP':
+            message = logout(data[1])
+
+        conn.sendall(message.encode())
+
 
 # Cria o soquete do servidor com o streaming e comeca a ouvir
 server_stream_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,12 +70,11 @@ server_client_socket.bind((HOST, SERVER_PORT))
 server_client_socket.listen()
 print('Esperando conexão do cliente...')
 
+
 # Aceita uma conexao com o cliente
 while True:
     client_connection, client_ip = server_client_socket.accept()
     print('GOT CONNECTION FROM:', client_ip)
+    connection_manager(client_connection)
 
-
-
-
-
+    connection_manager(stream_connection)
--- USR/main.py
+++ USR/main.py
@@ -1,27 +1,60 @@
 import socket
 
-LOCAL_HOST  = '127.0.0.1'   # localhost
-SERVER_HOST = '127.0.0.1'   # IP do servidor
-STREAM_HOST = '127.0.0.1'   # IP do stream
-CLIENT_PORT = 6060          # Porta do cliente
-SERVER_PORT = 5000          # Porta usada pelo servidor
-STREAM_PORT = 5050          # Porta onde o cliente vai se comunicar com o servidor de streaming
+LOCAL_HOST = '127.0.0.1'  # localhost
+SERVER_HOST = '127.0.0.1'  # IP do servidor
+STREAM_HOST = '127.0.0.1'  # IP do stream
+CLIENT_PORT = 6060  # Porta do cliente
+SERVER_PORT = 5000  # Porta usada pelo servidor
+STREAM_PORT = 5050  # Porta onde o cliente vai se comunicar com o servidor de streaming
+
+
+def login():
+    """
+    mostra na tela q o usuario foi cadastrado e entrar na app
+    :return:
+    """
+    return
+
+
+def status():
+    """
+    mostra na tela as informacoes recebidas do usuario
+    :return:
+    """
+    return
+
+
+def video_list():
+    """
+    lista os videos recebidos do servidor de streaming
+    :return:
+    """
+    return
+
 
 # Criacao e conexao do soquete do servidor
 client_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_server_socket.bind((LOCAL_HOST, CLIENT_PORT))
-client_server_socket.connect((STREAM_HOST, SERVER_PORT))
+client_server_socket.connect((SERVER_HOST, SERVER_PORT))
 
 # Criacao do soquete de comunicacao com o streaming
 client_streaming_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
-    pass
+# Tentativa de login
+message = 'ENTRAR_NA_APP' + ' ' + 'username' + ' ' + 'ip'  # Estou usando ' ' como separador dos arqgumentos
+client_server_socket.sendall(message.encode())
 
+isLogged = True
+while isLogged:
+    data_byte = client_server_socket.recv(1024)
+    data_string = data_byte.decode()
+    data = data_string.split(' ')
 
-
-
-
-
-
-
+    if data[0] == 'ENTRAR_NA_APP_ACK':
+        login()
+    elif data[0] == 'STATUS_DO_USUARIO':
+        status()
+    elif data[0] == 'SAIR_DA_APP_ACK':
+        isLogged = False
+    elif data[0] == 'LISTA_DE_VIDEOS':
+        video_list()
